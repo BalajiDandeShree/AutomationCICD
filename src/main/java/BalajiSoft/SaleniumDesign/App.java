ad360c7d5a4bf861ad13e6ddb099052db3b4c703
ShivanshDande <balajidande51@gmail.com>
2024-04-28 16:26:30 +0530

    initial files commit

---
+++ src/main/java/BalajiSoft/SaleniumDesign/App.java
@@ -0,0 +1,13 @@
+package BalajiSoft.SaleniumDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
